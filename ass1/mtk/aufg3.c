77072060355eb4d1e1ddb5fa780c94585e6560b7
FutureApp <michael-czaja-arbeit@hotmail.de>
2018-05-07 12:52:21 +0200

    mpi gibt rank fehler

--- ass1/mtk/aufg3.c
+++ ass1/mtk/aufg3.c
@@ -11,6 +11,7 @@ void mpirun();
 void recalculate();
 int world_size;
 int my_rank;
+int b[4];
 
 int main(int argc, char *argv[])
 {
@@ -31,7 +32,7 @@ int main(int argc, char *argv[])
 	 MPI_Get_processor_name(proc_name, &namelen);
 			 // finding out own computer name
 	getRandom();
-	// mpirun();
+	//mpirun();
 
 
 
@@ -42,10 +43,10 @@ int* getRandom() {
 			int m=10;
 			int r[m];
 			int i;
-			int *b={0};
-			b=(int*)malloc(4*sizeof(int));
+			int b[4]={0,0,0,0};
+//			b=(int*)malloc(4*sizeof(int));
 			int max=0;
-			int min=r[0];
+			int min;
 
 			/* set the seed */
 
@@ -56,77 +57,156 @@ int* getRandom() {
 				  if (r[i] > max)
 				              {
 				            	max = r[i];
-				            	b[0]=max;
+				            	//b[0]=max;
 				              }
 				  if (r[i] < min)
 				              {
 				        	 	min = r[i];
-				        	 	b[1]=r[i];
+				        	 	//b[1]=r[i];
 				              }
 				printf( "r[%d] = %d\n", i, r[i]);
 
 			}
 			b[3]=b[2]/m;
-
+			b[0]=max;
+			b[1]=min;
 			printf( "b[%d] = %d\n", 0, b[0]);
 			printf( "b[%d] = %d\n", 1, b[1]);
 			printf( "b[%d] = %d\n", 2, b[2]);
 			printf( "b[%d] = %d\n", 3, b[3]);
-			return b;
+//			return b;
+
+			int *received;
+			int *send;
+
+
+			int j;
+			for( j = 0; j <= world_size; j = j + 1 ){
+						if (my_rank==0){
+							MPI_Recv(&received, 4, MPI_INT, world_size, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+										if (b[0]>=received[0]) {  //maximum from both arrays
+											send[0]=b[0];
+											}
+											else{
+												send[0]=received[0];
+											}
+											if (b[1]>=received[1]) {  //minimum from both arrays
+													send[1]=received[1];
+												}
+												else{
+													send[1]=b[1];
+												}
+											send[2]=(b[2]+received[2]);
+											send[3]=(b[3]+received[3])/2;
+
+										MPI_Send(&send, 4, MPI_INT, my_rank +1, 1, MPI_COMM_WORLD);
+
+							}
+						if (my_rank==world_size){
+							MPI_Recv(&received, 4, MPI_INT, my_rank -1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+										if (b[0]>=received[0]) {  //maximum from both arrays
+											send[0]=b[0];
+											}
+											else{
+												send[0]=received[0];
+											}
+											if (b[1]>=received[1]) {  //minimum from both arrays
+													send[1]=received[1];
+												}
+												else{
+													send[1]=b[1];
+												}
+											send[2]=(b[2]+received[2]);
+											send[3]=(b[3]+received[3])/2;
+
+										MPI_Send(&send, 4, MPI_INT, 0, 1, MPI_COMM_WORLD);
+						}
+						else{
+							MPI_Recv(&received, 4, MPI_INT, my_rank -1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+										if (b[0]>=received[0]) {  //maximum from both arrays
+											send[0]=b[0];
+											}
+											else{
+												send[0]=received[0];
+											}
+											if (b[1]>=received[1]) {  //minimum from both arrays
+													send[1]=received[1];
+												}
+												else{
+													send[1]=b[1];
+												}
+											send[2]=(b[2]+received[2]);
+											send[3]=(b[3]+received[3])/2;
+
+										MPI_Send(&send, 4, MPI_INT, my_rank +1, 1, MPI_COMM_WORLD);
+						}
+					}
+
+
+
+
+
+
+
+
 }
+
 
 
 void mpirun(){
 
 
-		int a;
-		int *send;
-	//	b=getRandom();
-		int *received;
-		for( a = 0; a <= world_size; a = a + 1 ){
-			if (my_rank==0){
-				MPI_Recv(&received, 4, MPI_INT, world_size, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-				MPI_Send(&send, 4, MPI_INT, my_rank+1, 1, MPI_COMM_WORLD);
-				printf("von 0 erhalten");
-
-				}
-			if (my_rank==world_size){
-						MPI_Recv(&received, 4, MPI_INT, world_size, my_rank -1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-						MPI_Send(&send, 4, MPI_INT, 0, 1, MPI_COMM_WORLD);
-				printf("von world_size erhalten");
-			}
-			else{
-				MPI_Recv(&received, 4, MPI_INT, my_rank -1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-				MPI_Send(&send, 4, MPI_INT, my_rank +1, 1, MPI_COMM_WORLD);
-				printf("von node erhalten");
-			}
-		}
+//		int a;
+//		int *send;
+//	//	b=getRandom();
+//		int *received;
+//		for( a = 0; a <= world_size; a = a + 1 ){
+//			if (my_rank==0){
+//				MPI_Recv(&received, 4, MPI_INT, world_size, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//				MPI_Send(&send, 4, MPI_INT, my_rank+1, 1, MPI_COMM_WORLD);
+//				printf("von 0 erhalten");
+//
+//				}
+//			if (my_rank==world_size){
+//						MPI_Recv(&received, 4, MPI_INT, world_size, my_rank -1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//						MPI_Send(&send, 4, MPI_INT, 0, 1, MPI_COMM_WORLD);
+//				printf("von world_size erhalten");
+//			}
+//			else{
+//				MPI_Recv(&received, 4, MPI_INT, my_rank -1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//				MPI_Send(&send, 4, MPI_INT, my_rank +1, 1, MPI_COMM_WORLD);
+//				printf("von node erhalten");
+//			}
+//		}
 
 
 }
 
 
 void recalculate(){
-	int *a;
-	a=getRandom();
-	int *received;
-	int *send;
-//			MPI_Recv(&received, 4, MPI_INT, my_rank -1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	if (a[0]>=received[0]) {  //maximum from both arrays
-		send[0]=a[0];
-	}
-	else{
-		send[0]=received[0];
-	}
-	if (a[1]>=received[1]) {  //minimum from both arrays
-			send[1]=received[1];
-		}
-		else{
-			send[1]=a[1];
-		}
-	send[2]=(a[2]+received[2]);
-	send[3]=(a[3]+received[3])/2;
-//			MPI_Send(&send, 4, MPI_INT, my_rank +1, 1, MPI_COMM_WORLD);
+//	int *a;
+//	a=getRandom();
+//	int *received;
+//	int *send;
+//	MPI_Recv(&received, 4, MPI_INT, my_rank -1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//	if (a[0]>=received[0]) {  //maximum from both arrays
+//		send[0]=a[0];
+//	}
+//	else{
+//		send[0]=received[0];
+//	}
+//	if (a[1]>=received[1]) {  //minimum from both arrays
+//			send[1]=received[1];
+//		}
+//		else{
+//			send[1]=a[1];
+//		}
+//	send[2]=(a[2]+received[2]);
+//	send[3]=(a[3]+received[3])/2;
+////			MPI_Send(&send, 4, MPI_INT, my_rank +1, 1, MPI_COMM_WORLD);
 
 }
 
